a7ba62649d2f2dce738b5e13b57d56d09a5e40b1
任跃兵 <cnryb@live.cn>
2018-05-22 22:51:49 +0800

    添加项目文件。

---
+++ DotnetCoreSeleniumTestOnDocker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotnetCoreSeleniumTestOnDocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
